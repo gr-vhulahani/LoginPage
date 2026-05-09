6bc469899bfdad0a6a0ea04774bbb7109b381309
ronewav <Ronewavhulahani123@gmail.com>
2026-05-09 02:48:59 +0200

    Psuedocode and Adding of simple variables together with user input.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        //Variables
+
+        String user_name;
+        String user_pass;
+
+        System.out.print("Enter your username: ");
+        user_name = scanner.nextLine();
+
+        System.out.print("Enter your password: ");
+        user_pass = scanner.nextLine();
+
     }
 }
---
+++ src/pseudocode.md
@@ -0,0 +1,14 @@
+# Variables
+
+String user_name
+String user_password
+
+# Input
+- receive input from user
+- save inputs into already declared variables
+- grant access to the user after validation
+
+# Output
+- Display "Login Successful"
+
+
